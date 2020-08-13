fc15b57d1e686993212658af6422ec19fba1ac5b
Yash Adulkar <yash151099@gmail.com>
2020-08-13 21:38:09 +0530

    Change Notifiers

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,24 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PageOffsetNotifier with ChangeNotifier {
+  double _offset = 0;
+  double _page = 0;
+
+  PageOffsetNotifier(PageController pageController) {
+    pageController.addListener(() {
+      _offset = pageController.offset;
+      _page = pageController.page;
+      notifyListeners();
+    });
+  }
+
+  double get offset => _offset;
+
+  double get page => _page;
+}
 
 class MainPage extends StatelessWidget {
   final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            controller: _pageController,
-            physics: ClampingScrollPhysics(),
-            children: [
-              LeopardPage(),
-              VulturePage(),
-            ],
-          ),
-          Image.asset("assets/leopard.png")
-        ],
+    return ChangeNotifierProvider(
+      create: (_) => PageOffsetNotifier(_pageController),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              physics: ClampingScrollPhysics(),
+              children: [
+                LeopardPage(),
+                VulturePage(),
+              ],
+            ),
+            LeopardImage(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class LeopardImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/leopard.png");
   }
 }
 
